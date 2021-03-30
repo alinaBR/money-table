405cf03c03770d2856d735260168ca6f46c8c257
alinaBR <75022064+alinaBR@users.noreply.github.com>
2021-03-30 13:24:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 let money = prompt("Ваш бюджет на месяц?", ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
@@ -14,7 +15,7 @@ for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
         b = prompt("Во сколько обойдется", '');
 
-    if (typeof (a))=== 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
+    if ( typeof(a)=== 'string' && typeof (a) != null && typeof (b) != null && a != "" && b != "" && a.length < 50) {
         console.log("done");
         appData.expenses[a] = b;
     } else {
